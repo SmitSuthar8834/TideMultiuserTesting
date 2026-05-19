5b43e91d39e580062be90836bc63571264508120
SmitSuthar8834 <SmitSuthar8834>
2026-05-19 13:22:30 +0530

    Link repository with application Creatio Web Intelligence

---
+++ packages/UsrCreatioWebIntellige/Schemas/UsrCreatioWebIntellige_Detail/UsrCreatioWebIntellige_Detail.js
@@ -0,0 +1,8 @@
+define('UsrCreatioWebIntellige_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrCreatioWebIntellige',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
