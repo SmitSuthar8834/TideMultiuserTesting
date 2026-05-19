6fa4146f96d7b13225728f69441f8dec16d2e249
Smit Suthar <smitsuthar.techmihirnaik@gmail.com>
2026-05-19 13:24:44 +0530

    Delete packages/UsrCreatioWebIntellige directory

--- packages/UsrCreatioWebIntellige/Schemas/UsrCreatioWebIntellige_Detail/UsrCreatioWebIntellige_Detail.js
+++
@@ -1,8 +0,0 @@
-define('UsrCreatioWebIntellige_Detail', [], function () {
-	return {
-		entitySchemaName: 'UsrCreatioWebIntellige',
-		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
-		methods: {},
-	};
-});
